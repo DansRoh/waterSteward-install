ea540739f4690057789aee9a8db8c38e1833d34b
DansRoh <100189192+DansRoh@users.noreply.github.com>
2023-05-16 17:32:23 +0800

    fix组件AtForm小程序不正常显示的问题

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from "path";
 
 const config = {
   projectName: "waterSteward-install",
@@ -18,12 +18,18 @@ const config = {
     options: {},
   },
   framework: "react",
-  compiler: "webpack5",
+  // compiler: "webpack5",
+  compiler: {
+    type: "webpack5",
+    prebundle: {
+      exclude: ["taro-ui"],
+    },
+  },
   cache: {
     enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   alias: {
-    "@/static": path.resolve(__dirname, '..', "src/static"),
+    "@/static": path.resolve(__dirname, "..", "src/static"),
   },
   mini: {
     postcss: {
