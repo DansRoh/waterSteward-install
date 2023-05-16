b072e7cb2845ff99c763b6ef08ae9302feba6c98
DansRoh <100189192+DansRoh@users.noreply.github.com>
2023-05-16 10:09:06 +0800

    项目引入taroui组件库

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
 import './index.styl'
 
 export default function Index() {
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <View className='index'>
       <Text>Hello world!</Text>
+      <AtButton type='primary'>按钮</AtButton>
     </View>
   )
 }
