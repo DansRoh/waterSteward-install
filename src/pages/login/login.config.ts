8fa3683a84e6d4c91fade99b3736b980edfeb695
DansRoh <100189192+DansRoh@users.noreply.github.com>
2023-05-16 14:49:33 +0800

    login静态页面

---
+++ src/pages/login/login.config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: luoda
+ * @Date: 2023-05-16 10:11:34
+ * @LastEditTime: 2023-05-16 10:11:34
+ * @LastEditors: luoda
+ * @Description: 
+ */
+
+export default definePageConfig({
+  navigationBarTitleText: '鲜水安装'
+})
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,30 @@
+/*
+ * @Author: luoda
+ * @Date: 2023-05-16 10:11:08
+ * @LastEditTime: 2023-05-16 14:47:13
+ * @LastEditors: luoda
+ * @Description:
+ */
+
+import { Image, View } from "@tarojs/components";
+import logoImg from "@/static/images/01_logo.png";
+import wechatImg from "@/static/images/02_wechat.png";
+import phoneImg from "@/static/images/03_phone.png";
+import { AtButton } from "taro-ui";
+import "./login.styl";
+
+export default function login() {
+  return (
+    <View className="login-page">
+      <Image style="width:328rpx;height:136rpx;" src={logoImg}></Image>
+      <AtButton className="wxlogin-btn" circle>
+        <Image className="btn-icon" src={wechatImg}></Image>
+        微信授权登录
+      </AtButton>
+      <AtButton type="secondary" circle>
+        <Image className="btn-icon" src={phoneImg}></Image>
+        手机快捷登录
+      </AtButton>
+    </View>
+  );
+}
